1881db406ca4742e5b98dfe4b20221da2dd74d23
Zeynep Güneş <64778572+zgunes@users.noreply.github.com>
2023-04-26 22:56:08 +0300

    Add files via upload

---
+++ odev1.php
@@ -0,0 +1,14 @@
+<?php
+$sayi1=17;
+$sayi2=19;
+$sayi3=15;
+if($sayi1>$sayi2 && $sayi1>$sayi3){
+    echo "1.sayı büyük";
+}
+elseif($sayi2>$sayi1 && $sayi2>$sayi3){
+    echo "2.sayı büyük";
+}
+else{
+    echo "3.sayı büyük";
+}
+?>
